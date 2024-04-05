d6e4437bd34b6d128c9b2606c36b688c0990abb4
Arseniy Belkin <arseniibelkin@gmail.com>
2024-04-05 22:17:15 +0300

    evening commit

--- my_site/users/forms.py
+++ my_site/users/forms.py
@@ -1,17 +1,20 @@
 from django import forms
 from .models import User
 
+
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Repeat password", widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'city']
+        fields = [
+            "username",
+            "email",
+        ]
 
     def clean_password2(self):
         cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
+        if cd["password"] != cd["password2"]:
             raise forms.ValidationError("Passwords don't match.")
-        return cd['password2']
-    
+        return cd["password2"]
--- my_site/users/urls.py
+++ my_site/users/urls.py
@@ -4,18 +4,37 @@ from . import views
 
 urlpatterns = [
     # url-адреса входа и выхода
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     # url-адреса смены пароля
-    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-
+    path(
+        "password-change/",
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "password-change/done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
     # url-адреса сброса пароля
-    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password-reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
-    path('register/', views.register, name='register'),
+    path(
+        "password-reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset/<uidb64>/<token>",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
+    path("register/", views.register, name="register"),
 ]
